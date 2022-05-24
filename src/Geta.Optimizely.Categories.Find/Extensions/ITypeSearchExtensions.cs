8ea6d4764d5177880cc7317739e72ba9e76ec820
Brian Weeteling <brianweeteling@gmail.com>
2022-05-24 15:58:46 +0200

    Add code for Find

---
+++ src/Geta.Optimizely.Categories.Find/Extensions/ITypeSearchExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using EPiServer.Core;
+using EPiServer.Find;
+
+namespace Geta.EpiCategories.Find.Extensions
+{
+    public static class ITypeSearchExtensions
+    {
+        public static ITypeSearch<T> FilterByCategories<T>(this ITypeSearch<T> search, IEnumerable<ContentReference> categories) where T : ICategorizableContent
+        {
+            if (categories == null || categories.Any() == false)
+            {
+                return search;
+            }
+
+            return search.Filter(x => x.Categories().In(categories));
+        }
+
+        public static ITypeSearch<T> FilterHitsByCategories<T>(this ITypeSearch<T> search, IEnumerable<ContentReference> categories) where T : ICategorizableContent
+        {
+            if (categories == null || categories.Any() == false)
+            {
+                return search;
+            }
+
+            return search.FilterHits(x => x.Categories().In(categories));
+        }
+
+        public static ITypeSearch<T> ContentCategoriesFacet<T>(this ITypeSearch<T> request) where T : ICategorizableContent
+        {
+            return request.ContentReferenceFacet(x => x.Categories());
+        }
+
+        public static ITypeSearch<T> ContentReferenceFacet<T>(this ITypeSearch<T> request, Expression<Func<T, IEnumerable<string>>> fieldExpression) where T : ICategorizableContent
+        {
+            return request.TermsFacetFor(fieldExpression);
+        }
+    }
+}
---
+++ src/Geta.Optimizely.Categories.Find/FindCategoriesSearchProvider.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.Find.Cms.SearchProviders;
+using EPiServer.Framework.Localization;
+using EPiServer.Shell;
+using EPiServer.Shell.Search;
+using EPiServer.Web;
+
+namespace Geta.EpiCategories.Find
+{
+    [SearchProvider]
+    public class FindCategoriesSearchProvider : EnterpriseContentSearchProviderBase<CategoryData, ContentType>
+    {
+        private readonly LocalizationService _localizationService;
+        private readonly ISiteDefinitionResolver _siteDefinitionResolver;
+        private readonly IEnumerable<IContentRepositoryDescriptor> _contentRepositoryDescriptors;
+
+        public FindCategoriesSearchProvider(LocalizationService localizationService, ISiteDefinitionResolver siteDefinitionResolver, IContentTypeRepository contentTypeRepository, UIDescriptorRegistry uiDescriptorRegistry, IEnumerable<IContentRepositoryDescriptor> contentRepositoryDescriptors) : base(localizationService, siteDefinitionResolver, contentTypeRepository, uiDescriptorRegistry)
+        {
+            _localizationService = localizationService;
+            _siteDefinitionResolver = siteDefinitionResolver;
+            _contentRepositoryDescriptors = contentRepositoryDescriptors;
+        }
+
+        protected override string ToolTipResourceKeyBase => "/shell/cms/search/pages/tooltip";
+
+        protected override string ToolTipContentTypeNameResourceKey => "contenttype";
+
+        public override string Area => "CMS/categories";
+
+        public override string Category => _localizationService.GetString("/cms/searchprovider/findcategories/name", "Find categories");
+
+        public override IEnumerable<SearchResult> Search(Query query)
+        {
+            query.SearchRoots = _contentRepositoryDescriptors
+                .First(x => x.Key == CategoryContentRepositoryDescriptor.RepositoryKey)
+                .Roots
+                .Select(x => x.ToReferenceWithoutVersion().ToString());
+
+            return base.Search(query);
+        }
+
+        protected override string GetEditUrl(CategoryData contentData, out bool onCurrentHost)
+        {
+            ContentReference contentLink = contentData.ContentLink;
+            string language = contentData.Language.Name;
+            string editPath = EditPath(contentData, contentLink, language);
+            onCurrentHost = true;
+
+            var contentSiteDefinition = _siteDefinitionResolver.GetByContent(contentLink, true, true);
+
+            if (contentSiteDefinition.SiteUrl == SiteDefinition.Current.SiteUrl)
+                return editPath;
+
+            onCurrentHost = false;
+            return editPath;
+        }
+
+        protected override string IconCssClass(CategoryData contentData)
+        {
+            return "epi-resourceIcon epi-resourceIcon-category";
+        }
+    }
+}
